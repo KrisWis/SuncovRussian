5fcc6e75cf2b9deaef6ee10e0947e3e15ca75c49
KrisWis <kworkEvgeniy@yandex.ru>
2024-12-26 16:18:43 +0300

    Bugfix.

--- src/widgets/TestsWords/ui/TestsWords.tsx
+++ src/widgets/TestsWords/ui/TestsWords.tsx
@@ -18,7 +18,7 @@ import {
   useWords,
 } from '..';
 import { useTestsWordsActions } from '../model/slice/slice';
-import { shuffleArray } from '@/shared/utils/shuffleArray/shuffleArray';
+import { TestsWordsInterface } from '@/shared/static/tests_words/types';
 
 // TODO: написать тесты
 // TODO: подумать по поводу энтити адаптер
@@ -52,28 +52,39 @@ const TestsWordsInner: React.FC<TestsWordsProps> = memo(
     // Получение случайного слова
     const storeWords = useWords();
 
-    const updateRandomWord = useCallback(() => {
-      const storeWordsCopy = storeWords.filter(
-        (word) => word.id !== randomWordId,
-      );
+    const updateRandomWord = useCallback(
+      (words?: TestsWordsInterface[]) => {
+        let storeWordsCopy;
 
-      if (storeWordsCopy.length === 0) return;
-
-      const totalChances = storeWordsCopy.reduce(
-        (acc, c) => acc + c.probability!,
-        0,
-      );
-      const rnd = totalChances * Math.random();
-
-      for (let i = 0, sum = 0; ; i++) {
-        sum += storeWordsCopy[i].probability!;
-
-        if (sum > rnd) {
-          setRandomWordId(storeWordsCopy[i].id);
-          return;
+        if (words) {
+          storeWordsCopy = words.filter((word) => word.id !== randomWordId);
+        } else {
+          storeWordsCopy = storeWords.filter(
+            (word) => word.id !== randomWordId,
+          );
         }
-      }
-    }, [randomWordId, storeWords]);
+
+        setRandomWordsIsReverse((prev) => !prev);
+
+        if (storeWordsCopy.length === 0) return;
+
+        const totalChances = storeWordsCopy.reduce(
+          (acc, c) => acc + c.probability!,
+          0,
+        );
+        const rnd = totalChances * Math.random();
+
+        for (let i = 0, sum = 0; ; i++) {
+          sum += storeWordsCopy[i].probability!;
+
+          if (sum > rnd) {
+            setRandomWordId(storeWordsCopy[i].id);
+            return;
+          }
+        }
+      },
+      [randomWordId, storeWords],
+    );
 
     useEffect(() => {
       if (storeWords.length && randomWordId === null) {
@@ -115,180 +126,182 @@ const TestsWordsInner: React.FC<TestsWordsProps> = memo(
       setWords(UpdatedWordsWithUncorrectTimes);
       setTotalTime(0);
 
-      updateRandomWord();
+      updateRandomWord(UpdatedWordsWithUncorrectTimes);
     }, [setTotalTime, setWords, updateRandomWord, wordsWithUncorrectTimes]);
 
     // Изменение вероятности при неправильном ответе
     const [waitRepeatedClickInFail, setWaitRepeatedClickInFail] =
       useState<boolean>(false);
 
-    const wordOnFail = useCallback(() => {
-      if (waitRepeatedClickInFail) return;
+    const wordOnFail = useCallback(
+      (words: TestsWordsInterface[]) => {
+        if (waitRepeatedClickInFail) return;
 
-      const audio = new Audio('sounds/FailSound.mp3');
-      audio.play();
+        const audio = new Audio('sounds/FailSound.mp3');
+        audio.play();
 
-      setIsIncorrect(true);
+        setIsIncorrect(true);
 
-      setWaitRepeatedClickInFail(true);
+        setWaitRepeatedClickInFail(true);
 
-      const showNewWord = () => {
-        setWaitRepeatedClickInFail(false);
-        setIsIncorrect(false);
+        const showNewWord = () => {
+          setWaitRepeatedClickInFail(false);
+          setIsIncorrect(false);
 
-        const currentRandomWord = storeWords.find(
+          const currentRandomWord = words.find(
+            (word) => word.id === randomWordId,
+          );
+
+          if (!isErrorWork) {
+            changeWordProbability({
+              probability: 0.2,
+              id: currentRandomWord!.id,
+            });
+
+            changeWordUncorrectTimes({
+              id: currentRandomWord!.id,
+              uncorrectTimes: currentRandomWord!.uncorrectTimes! + 1,
+            });
+          }
+
+          changeWordConsecutivelyTimes({
+            id: currentRandomWord!.id,
+            consecutivelyTimes: 0,
+          });
+
+          changeWordInProgressStatus({
+            id: currentRandomWord!.id,
+            inProgress: false,
+          });
+
+          updateRandomWord();
+
+          document.querySelector('body')!.style.pointerEvents = 'all';
+          document.removeEventListener('click', showNewWord);
+        };
+
+        const eventTimeout = setTimeout(() => {
+          document.querySelector('body')!.style.pointerEvents = 'none';
+          document.addEventListener('click', showNewWord);
+          clearTimeout(eventTimeout);
+        }, 0);
+      },
+      [
+        changeWordConsecutivelyTimes,
+        changeWordInProgressStatus,
+        changeWordProbability,
+        changeWordUncorrectTimes,
+        isErrorWork,
+        randomWordId,
+        updateRandomWord,
+        waitRepeatedClickInFail,
+      ],
+    );
+
+    // Изменение вероятности при правильном ответе
+    const wordOnSuccess = useCallback(
+      (words: TestsWordsInterface[]) => {
+        if (waitRepeatedClickInFail) return;
+
+        const currentRandomWord = words.find(
           (word) => word.id === randomWordId,
         );
 
-        if (!isErrorWork) {
-          changeWordProbability({
-            probability: 0.2,
+        if (isErrorWork) {
+          const futureConsecutivelyTimes =
+            currentRandomWord!.consecutivelyTimes! + 1;
+
+          changeWordConsecutivelyTimes({
             id: currentRandomWord!.id,
+            consecutivelyTimes: futureConsecutivelyTimes,
           });
 
-          changeWordUncorrectTimes({
-            id: currentRandomWord!.id,
-            uncorrectTimes: currentRandomWord!.uncorrectTimes! + 1,
-          });
+          if (futureConsecutivelyTimes === 3) {
+            changeWordInProgressStatus({
+              id: currentRandomWord!.id,
+              inProgress: true,
+            });
+
+            changeWordProbability({
+              probability: 0.05,
+              id: currentRandomWord!.id,
+            });
+          }
+        } else {
+          if (currentRandomWord!.probability === 0.2) {
+            changeWordProbability({
+              id: currentRandomWord!.id,
+              probability: 0.1,
+            });
+          } else if (currentRandomWord!.probability === 0.1) {
+            changeWordProbability({
+              id: currentRandomWord!.id,
+              probability: 0.05,
+            });
+          } else {
+            changeWordProbability({
+              id: currentRandomWord!.id,
+              probability: 0.01,
+            });
+
+            changeWordInProgressStatus({
+              id: currentRandomWord!.id,
+              inProgress: true,
+            });
+          }
         }
-
-        changeWordConsecutivelyTimes({
-          id: currentRandomWord!.id,
-          consecutivelyTimes: 0,
-        });
-
-        changeWordInProgressStatus({
-          id: currentRandomWord!.id,
-          inProgress: false,
-        });
 
         updateRandomWord();
-
-        document.querySelector('body')!.style.pointerEvents = 'all';
-        document.removeEventListener('click', showNewWord);
-      };
-
-      const eventTimeout = setTimeout(() => {
-        document.querySelector('body')!.style.pointerEvents = 'none';
-        document.addEventListener('click', showNewWord);
-        clearTimeout(eventTimeout);
-      }, 0);
-    }, [
-      changeWordConsecutivelyTimes,
-      changeWordInProgressStatus,
-      changeWordProbability,
-      changeWordUncorrectTimes,
-      isErrorWork,
-      randomWordId,
-      storeWords,
-      updateRandomWord,
-      waitRepeatedClickInFail,
-    ]);
-
-    // Изменение вероятности при правильном ответе
-    const wordOnSuccess = useCallback(() => {
-      if (waitRepeatedClickInFail) return;
-
-      const currentRandomWord = storeWords.find(
-        (word) => word.id === randomWordId,
-      );
-
-      if (isErrorWork) {
-        const futureConsecutivelyTimes =
-          currentRandomWord!.consecutivelyTimes! + 1;
-
-        changeWordConsecutivelyTimes({
-          id: currentRandomWord!.id,
-          consecutivelyTimes: futureConsecutivelyTimes,
-        });
-
-        if (futureConsecutivelyTimes === 3) {
-          changeWordInProgressStatus({
-            id: currentRandomWord!.id,
-            inProgress: true,
-          });
-
-          changeWordProbability({
-            probability: 0.05,
-            id: currentRandomWord!.id,
-          });
-        }
-      } else {
-        if (currentRandomWord!.probability === 0.2) {
-          changeWordProbability({
-            id: currentRandomWord!.id,
-            probability: 0.1,
-          });
-        } else if (currentRandomWord!.probability === 0.1) {
-          changeWordProbability({
-            id: currentRandomWord!.id,
-            probability: 0.05,
-          });
-        } else {
-          changeWordProbability({
-            id: currentRandomWord!.id,
-            probability: 0.01,
-          });
-
-          changeWordInProgressStatus({
-            id: currentRandomWord!.id,
-            inProgress: true,
-          });
-        }
-      }
-
-      updateRandomWord();
-    }, [
-      changeWordConsecutivelyTimes,
-      changeWordInProgressStatus,
-      changeWordProbability,
-      isErrorWork,
-      randomWordId,
-      storeWords,
-      updateRandomWord,
-      waitRepeatedClickInFail,
-    ]);
+      },
+      [
+        changeWordConsecutivelyTimes,
+        changeWordInProgressStatus,
+        changeWordProbability,
+        isErrorWork,
+        randomWordId,
+        updateRandomWord,
+        waitRepeatedClickInFail,
+      ],
+    );
 
     // Отображение слов в случайном порядке
-    const [randomWords, setRandomWords] = useState<React.JSX.Element[]>([]);
+    const [randomWordsIsReverse, setRandomWordsIsReverse] =
+      useState<boolean>(false);
 
-    useEffect(() => {
-      const randomWord = storeWords.find((word) => word.id === randomWordId);
-
-      if (!randomWord) return;
-
-      const randomWords = shuffleArray([
-        <Flex
-          key={randomWord.valid}
-          onClick={wordOnSuccess}
-          className={styles.TestsWords__word}
-          justify="center"
-        >
-          {randomWord.valid}
-        </Flex>,
-
-        <Flex
-          key={randomWord.invalid}
-          onClick={wordOnFail}
-          className={styles.TestsWords__word}
-          justify="center"
-        >
-          {randomWord.invalid}
-        </Flex>,
-      ]);
-
-      setRandomWords(randomWords);
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [randomWordId]);
+    const randomWord = useMemo(
+      () => storeWords.find((word) => word.id === randomWordId),
+      [randomWordId, storeWords],
+    );
 
     return (
       <>
         {!totalTime ? (
           <>
             {isIncorrect && <Flex justify="center">Неверно</Flex>}
-            {randomWords && (
-              <Flex justify="center" width="100">
-                {randomWords.map((word) => word)}
+            {randomWord && (
+              <Flex
+                width="100"
+                justify="center"
+                align="center"
+                direction={randomWordsIsReverse ? 'rowReverse' : 'row'}
+              >
+                <Flex
+                  key={randomWord.valid}
+                  onClick={() => wordOnSuccess(storeWords)}
+                  className={styles.TestsWords__word}
+                  justify="center"
+                >
+                  {randomWord.valid}
+                </Flex>
+
+                <Flex
+                  key={randomWord.invalid}
+                  onClick={() => wordOnFail(storeWords)}
+                  className={styles.TestsWords__word}
+                  justify="center"
+                >
+                  {randomWord.invalid}
+                </Flex>
               </Flex>
             )}
 
