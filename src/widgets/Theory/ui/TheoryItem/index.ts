3fed612c367155368c3249941e88baf2063912b9
KrisWis <kworkEvgeniy@yandex.ru>
2025-01-14 23:05:33 +0300

    Resolve conflicts by taking incoming changes

--- src/widgets/Theory/ui/TheoryItem/index.ts
+++ src/widgets/Theory/ui/TheoryItem/index.ts
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export { TheoryItem } from './ui/TheoryItem';
+=======
+export { TheoryItem } from './ui/TheoryItem';
+>>>>>>> 4fa07a1 (Replace yandex cloud api to uploadthingapi, rewrite api requests, add dotenvplugin for webpack. Theory is finished, but have some details to fix.)
--- src/widgets/Theory/ui/TheoryItem/ui/TheoryItem.tsx
+++ src/widgets/Theory/ui/TheoryItem/ui/TheoryItem.tsx
@@ -8,10 +8,17 @@ import { ErrorComponent } from '@/shared/ui-kit/ErrorComponent';
 export const TheoryItem: React.FC<TheoryItemProps> = memo(
   ({ fileKey }): React.JSX.Element => {
     // Получение ссылки на файл по ключу
+<<<<<<< HEAD
     const { data, isLoading, isError, isFetching } = useGetTheoryQuery(fileKey);
 
     // Обработка события загрузки файлов
     if (isLoading || isFetching) {
+=======
+    const { data, isLoading, isError } = useGetTheoryQuery(fileKey);
+
+    // Обработка события загрузки файлов
+    if (isLoading) {
+>>>>>>> 4fa07a1 (Replace yandex cloud api to uploadthingapi, rewrite api requests, add dotenvplugin for webpack. Theory is finished, but have some details to fix.)
       return <PageLoading />;
     }
 
@@ -20,11 +27,15 @@ export const TheoryItem: React.FC<TheoryItemProps> = memo(
       return <ErrorComponent />;
     }
 
+<<<<<<< HEAD
     return (
       <div data-testid="TheoryItem">
         {data?.fileData && <PDFViewer url={data?.fileData.fileUrl} />}
       </div>
     );
+=======
+    return <>{data?.fileData && <PDFViewer url={data?.fileData.fileUrl} />}</>;
+>>>>>>> 4fa07a1 (Replace yandex cloud api to uploadthingapi, rewrite api requests, add dotenvplugin for webpack. Theory is finished, but have some details to fix.)
   },
 );
 
