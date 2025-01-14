622c9db8e8959dfa48e9221cc8612259eab07297
KrisWis <kworkEvgeniy@yandex.ru>
2025-01-14 23:05:17 +0300

    Resolve conflicts by taking incoming changes

--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -3,7 +3,11 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 export function buildCssLoader(isDev: boolean, isStorybook: boolean) {
     return {
         test: /\.(sa|sc|c)ss$/i,
+<<<<<<< HEAD
         exclude: isStorybook ? /node_modules/ : /node_modules\/(?!react-pdf)/,
+=======
+        exclude: /node_modules\/(?!react-pdf)/,
+>>>>>>> 12d4b89 (Delete cnd package for pdf and add react alternative lib.)
         use: [
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
