ab1d5ea5697202c1c7704d30d7b3d588761b0c53
KrisWis <kworkEvgeniy@yandex.ru>
2025-01-14 23:07:25 +0300

    Resolve conflicts by taking incoming changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,13 @@
-<<<<<<< HEAD
-import { MainPage } from '@/pages/MainPage';
-import { PageLoading } from '@/shared/ui-kit/PageLoading/PageLoading';
-import { Suspense } from 'react';
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import { AppRouter } from './providers/router';
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom';
->>>>>>> 93ebe07 (Fully initialize router, add routing for all project, add unit and e2e tests for routing, refactore project - made sections as pages.)
-=======
 
->>>>>>> c31c805 (bugfix)
+<<<<<<< HEAD
 // TODO: Исправить ошибку с creevey, либо найти новую либу для ui тестирования со Storybook
-=======
-// TODO: написать cypress тесты в конце разработки
->>>>>>> 3736a6b (Add SideBar for Theory Block, add Theory Context and add functional of switching sections of theories.)
+
+// TODO: Исправить ошибку с creevey, либо найти новую либу для ui тестирования со Storybook
 =======
 // TODO: написать cypress тесты
-=======
->>>>>>> 04df36d (Add cypress, e2e tests for theory block.)
 // TODO: найти библиотеку для скриншотного тестирования со Storybook и написать тесты
 >>>>>>> 1df825a (Remove loki from project.)
-=======
-// TODO: Исправить ошибку с creevey, либо найти новую либу для ui тестирования со Storybook
->>>>>>> 4d9286d (Add creevey instead of loki.)
 
 export const App = () => {
   return <AppRouter />;
