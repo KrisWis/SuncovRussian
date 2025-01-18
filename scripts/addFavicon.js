4027f1c0ee1567640b4c5f3c9b6b9377abb511a5
KrisWis <kworkEvgeniy@yandex.ru>
2025-01-18 12:50:25 +0300

    Updates

--- scripts/addFavicon.js
+++ scripts/addFavicon.js
@@ -1,1 +1,1 @@
-const timeout=setTimeout((()=>{const e=Boolean(document.body.getAttribute("data-isdev"))?"/favicon.ico":"/SuncovRussian/favicon.ico",t=document.createElement("link");t.rel="icon",t.type="image/x-icon",t.href=e,document.head.appendChild(t),clearTimeout(timeout)}),1e3);
+const timeout=setTimeout((()=>{const e=Boolean(document.body.getAttribute("data-isdev"));console.log(e);const o=e?"/favicon.ico":"/SuncovRussian/favicon.ico",t=document.createElement("link");t.rel="icon",t.type="image/x-icon",t.href=o,document.head.appendChild(t),clearTimeout(timeout)}),1e3);
