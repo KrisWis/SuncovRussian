a4df6389e1a743a6e7ec6a129a12e2839e9afa72
KrisWis <kworkEvgeniy@yandex.ru>
2025-02-01 23:48:46 +0300

    Fix trainerpage unit tests.

--- src/pages/TrainerPage/ui/TrainerPage.test.tsx
+++ src/pages/TrainerPage/ui/TrainerPage.test.tsx
@@ -25,22 +25,24 @@ type ModeTypes = 'Одна жизнь' | 'Проверка';
 // Helpers
 
 // Функция для проверки значения прогресс бара
-const checkProgressBarValue = (
+const checkProgressBarValue = async (
   expectedValue: number,
   component: RenderResult<typeof queries, HTMLElement, HTMLElement>,
   comparisonType: ComparisonType = 'equal',
 ) => {
-  const progressBar = component.getByTestId(
-    'Trainer__TrainerProgressBar__percent',
-  );
-
-  if (comparisonType === 'equal') {
-    expect(Number(progressBar.getAttribute('value'))).toBe(expectedValue);
-  } else if (comparisonType === 'greaterThan') {
-    expect(Number(progressBar.getAttribute('value'))).toBeGreaterThan(
-      expectedValue,
+  await waitFor(() => {
+    const progressBar = component.getByTestId(
+      'Trainer__TrainerProgressBar__percent',
     );
-  }
+
+    if (comparisonType === 'equal') {
+      expect(Number(progressBar.getAttribute('value'))).toBe(expectedValue);
+    } else if (comparisonType === 'greaterThan') {
+      expect(Number(progressBar.getAttribute('value'))).toBeGreaterThan(
+        expectedValue,
+      );
+    }
+  });
 };
 
 // Функция для имитации клика на слова и проверки появления плашки "Неверно"
@@ -67,6 +69,100 @@ const clickWordAndCheckUncorrectBar = async (
 };
 
 // Tests
+describe('PrimaryTrainerWords', () => {
+  // Helpers
+  const setupTest = () => {
+    return renderWithProviders(
+      <TrainerPage theme="Ударения" words={wordsForTrainers['Ударения']} />,
+      getRouteTrainer('udareniya'),
+    );
+  };
+
+  let component: RenderResult<typeof queries, HTMLElement, HTMLElement>;
+
+  beforeEach(async () => {
+    await waitFor(() => {
+      component = setupTest();
+    });
+  });
+
+  // Tests
+  test('Click valid words and not get an error, check progress bar', async () => {
+    await waitFor(async () => {
+      // Click valid words
+      await clickWordAndCheckUncorrectBar(
+        'PrimaryTrainerWords__valid',
+        false,
+        component,
+      );
+
+      await clickWordAndCheckUncorrectBar(
+        'PrimaryTrainerWords__valid',
+        false,
+        component,
+      );
+
+      await clickWordAndCheckUncorrectBar(
+        'PrimaryTrainerWords__valid',
+        false,
+        component,
+      );
+
+      // Progress bar value must increase
+      await checkProgressBarValue(0, component, 'greaterThan');
+    });
+  });
+
+  test('Click invalid word and valid word and get an error, check progress bar', async () => {
+    await waitFor(async () => {
+      // Click valid word
+      await clickWordAndCheckUncorrectBar(
+        'PrimaryTrainerWords__valid',
+        false,
+        component,
+      );
+
+      // Click invalid word
+      await clickWordAndCheckUncorrectBar(
+        'PrimaryTrainerWords__invalid',
+        true,
+        component,
+      );
+
+      // Progress bar value must increase, because valid word was clicked
+      await checkProgressBarValue(0, component, 'greaterThan');
+    });
+  });
+
+  test('Click invalid words, check progress bar', async () => {
+    // Click invalid words
+    await clickWordAndCheckUncorrectBar(
+      'PrimaryTrainerWords__invalid',
+      true,
+      component,
+    );
+
+    await clickWordAndCheckUncorrectBar(
+      'PrimaryTrainerWords__invalid',
+      true,
+      component,
+    );
+
+    await clickWordAndCheckUncorrectBar(
+      'PrimaryTrainerWords__invalid',
+      true,
+      component,
+    );
+
+    // Progress bar value must be equal zero
+    await checkProgressBarValue(0, component);
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+});
+
 describe('TrainerModeSwitcher', () => {
   // Helpers
   const setupTest = () => {
@@ -126,7 +222,7 @@ describe('TrainerModeSwitcher', () => {
     );
 
     // И прогресс бар должен обнулиться
-    checkProgressBarValue(0, component);
+    await checkProgressBarValue(0, component);
   });
 
   test('Click check mode, check progress bar', async () => {
@@ -159,101 +255,7 @@ describe('TrainerModeSwitcher', () => {
     );
 
     // И прогресс бар должен быть больше 0
-    checkProgressBarValue(0, component, 'greaterThan');
-  });
-
-  afterEach(() => {
-    cleanup();
-  });
-});
-
-describe('PrimaryTrainerWords', () => {
-  // Helpers
-  const setupTest = () => {
-    return renderWithProviders(
-      <TrainerPage theme="Ударения" words={wordsForTrainers['Ударения']} />,
-      getRouteTrainer('udareniya'),
-    );
-  };
-
-  let component: RenderResult<typeof queries, HTMLElement, HTMLElement>;
-
-  beforeEach(async () => {
-    await waitFor(() => {
-      component = setupTest();
-    });
-  });
-
-  // Tests
-  test('Click valid words and not get an error, check progress bar', async () => {
-    await waitFor(async () => {
-      // Click valid words
-      await clickWordAndCheckUncorrectBar(
-        'PrimaryTrainerWords__valid',
-        false,
-        component,
-      );
-
-      await clickWordAndCheckUncorrectBar(
-        'PrimaryTrainerWords__valid',
-        false,
-        component,
-      );
-
-      await clickWordAndCheckUncorrectBar(
-        'PrimaryTrainerWords__valid',
-        false,
-        component,
-      );
-
-      // Progress bar value must increase
-      checkProgressBarValue(0, component, 'greaterThan');
-    });
-  });
-
-  test('Click invalid word and valid word and get an error, check progress bar', async () => {
-    await waitFor(async () => {
-      // Click valid word
-      await clickWordAndCheckUncorrectBar(
-        'PrimaryTrainerWords__valid',
-        false,
-        component,
-      );
-
-      // Click invalid word
-      await clickWordAndCheckUncorrectBar(
-        'PrimaryTrainerWords__invalid',
-        true,
-        component,
-      );
-
-      // Progress bar value must increase, because valid word was clicked
-      checkProgressBarValue(0, component, 'greaterThan');
-    });
-  });
-
-  test('Click invalid words, check progress bar', async () => {
-    // Click invalid words
-    await clickWordAndCheckUncorrectBar(
-      'PrimaryTrainerWords__invalid',
-      true,
-      component,
-    );
-
-    await clickWordAndCheckUncorrectBar(
-      'PrimaryTrainerWords__invalid',
-      true,
-      component,
-    );
-
-    await clickWordAndCheckUncorrectBar(
-      'PrimaryTrainerWords__invalid',
-      true,
-      component,
-    );
-
-    // Progress bar value must be equal zero
-    checkProgressBarValue(0, component);
+    await checkProgressBarValue(0, component, 'greaterThan');
   });
 
   afterEach(() => {
@@ -296,41 +298,6 @@ describe('UnionsTrainerWords', () => {
   };
 
   // Tests
-  test('Click invalid words, check progress bar', async () => {
-    await waitFor(async () => {
-      // Get type of current word
-      const wordCurrentType = getTypeOfCurrentWord();
-
-      // Get opposite type of current word
-      const wordOppositeType: unionTypes =
-        wordCurrentType === 'Подчинительный'
-          ? 'Сочинительный'
-          : 'Подчинительный';
-
-      // Click invalid words
-      await clickWordAndCheckUncorrectBar(
-        `UnionsTrainerWords__${wordOppositeType}`,
-        true,
-        component,
-      );
-
-      await clickWordAndCheckUncorrectBar(
-        `UnionsTrainerWords__${wordOppositeType}`,
-        true,
-        component,
-      );
-
-      await clickWordAndCheckUncorrectBar(
-        `UnionsTrainerWords__${wordOppositeType}`,
-        true,
-        component,
-      );
-
-      // Progress bar value must be equal zero
-      checkProgressBarValue(0, component);
-    });
-  });
-
   test('Click valid words and not get an error, check progress bar', async () => {
     await waitFor(async () => {
       // Get type of current word
@@ -356,7 +323,7 @@ describe('UnionsTrainerWords', () => {
       );
 
       // Progress bar value must increase
-      checkProgressBarValue(0, component, 'greaterThan');
+      await checkProgressBarValue(0, component, 'greaterThan');
     });
   });
 
@@ -386,8 +353,33 @@ describe('UnionsTrainerWords', () => {
       );
 
       // Progress bar value must increase, because valid word was clicked
-      checkProgressBarValue(0, component, 'greaterThan');
+      await checkProgressBarValue(0, component, 'greaterThan');
     });
+  });
+
+  test('Click invalid words, check progress bar', async () => {
+    // Get type of current word
+    const wordCurrentType = getTypeOfCurrentWord();
+
+    // Get opposite type of current word
+    const wordOppositeType: unionTypes =
+      wordCurrentType === 'Подчинительный' ? 'Сочинительный' : 'Подчинительный';
+
+    // Click invalid words
+    await clickWordAndCheckUncorrectBar(
+      `UnionsTrainerWords__${wordOppositeType}`,
+      true,
+      component,
+    );
+
+    await clickWordAndCheckUncorrectBar(
+      `UnionsTrainerWords__${wordOppositeType}`,
+      true,
+      component,
+    );
+
+    // Progress bar value must be equal zero
+    await checkProgressBarValue(0, component);
   });
 
   afterEach(() => {
